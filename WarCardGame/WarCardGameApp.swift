a7b687b257d7ae3a49b548db36f097ce4cbcc8d1
Philip Keller <philipjoelkeller@gmail.com>
2023-03-06 14:46:57 -0800

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Philip Keller on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
